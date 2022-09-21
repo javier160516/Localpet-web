2dc03781048cc9b046a7e6f8bb4ebe73ea613471
Javier Delgado <javiercambranis23@gmail.com>
2022-09-21 16:51:14 -0500

    funcionalidad

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
         {messageError ? (<p className='w-full py-2 px-3 bg-red-600 text-white text-center'>{messageError}</p>) : null}
         <form action="" method="post" className='py-5' onSubmit={handleSubmit}>
           <div className='mb-3'>
-            <label htmlFor="email" className='block ml-2 mb-1 text-sm '>Correo Electrónico</label>
+            <label htmlFor="email" className='block ml-2 mb-1 text-sm font-semibold'>Correo Electrónico</label>
             <input
               className='w-full focus:border-orange-500 focus:outline-none p-2 border-solid border border-gray-300 rounded-3xl text-sm'
               type='email'
@@ -82,7 +82,7 @@ const Login = () => {
             {emailError ? <p className='ml-2 text-xs text-red-500 mt-2'>{emailError}</p> : null}
           </div>
           <div className='mb-6'>
-            <label htmlFor="password" className='block ml-2 mb-1 text-sm '>Contraseña</label>
+            <label htmlFor="password" className='block ml-2 mb-1 text-sm font-semibold'>Contraseña</label>
             <div className='relative'>
               <input
                 className='w-full focus:border-orange-500 focus:outline-none p-2 border-solid border border-gray-300 rounded-3xl text-sm'
@@ -110,11 +110,11 @@ const Login = () => {
           <div className='flex justify-center mt-5'>
             <button type='submit' className='hover:bg-orange-700 transition-all duration-300 bg-orange-500 px-8 py-1 rounded-xl text-white font-bold'>Iniciar Sesión</button>
           </div>
-          <div>
-            <Link className='hover:text-blue-600 transition-all duration-300 text-xs block text-center mt-4' to="/register">
+          <div className='flex flex-col items-center mt-4'>
+            <Link className='hover:text-orange-500 transition-all duration-300 text-xs text-center mt-2 mb-2' to="/register">
               ¿No tienes cuenta?, Registrate
             </Link>
-            <Link className='hover:text-blue-600 transition-all duration-300 text-xs block text-center mt-1' to="/forgot-password">
+            <Link className='hover:text-orange-500 transition-all duration-300 text-xs text-center mt-2 mb-2' to="/forgot-password">
               ¿Olvidaste tu contraseña?, Recuperala
             </Link>
           </div>
