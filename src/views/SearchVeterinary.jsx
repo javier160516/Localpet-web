e8155736831b3eb174bfbc9060856653933bdef5
Javier Delgado <javiercambranis23@gmail.com>
2022-10-05 11:10:05 -0500

    Se agreggó leaftlet

--- src/views/SearchVeterinary.jsx
+++ src/views/SearchVeterinary.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Header from '../components/templates/Header'
 import Footer from '../components/templates/Footer'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
-const SearchVeterinary = () => {
+// import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { TileLayer, Marker, Popup, MapContainer } from 'react-leaflet';
 
+const SearchVeterinary = () => {
+  const position = [21.138872, -86.834190];
   return (
     <div className='h-screen overflow-hidden'>
       <Header />
       <div className='bg-[#E5E9F2] py-5'>
         <h1 className='text-center text-orange-500 font-bold text-3xl uppercase'>Buscar Veterinaria</h1>
-        <div className='w-6/12 h-1/2'>
-          <MapContainer center={[21.138872, -86.834190]} zoom={13} scrollWheelZoom={false}>
+        <div className='w-10/12 my-5 mx-auto shadow-lg'>
+          <MapContainer style={{height: 450}} center={position} zoom={12} >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[21.138872, -86.834190]}>
+            <Marker position={position}>
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                este es un popover
               </Popup>
             </Marker>
           </MapContainer>
