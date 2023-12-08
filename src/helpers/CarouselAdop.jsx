0068302919cc205b58e9a9b9314dc6d1e2c8f764
WilmerC27 <l18530441@cancun.tecnm.mx>
2023-12-07 20:12:11 -0500

    update-adoption

--- src/helpers/CarouselAdop.jsx
+++ src/helpers/CarouselAdop.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import banner from "../assets/adoptions/perro1.jpeg";
-import banner2 from "../assets/adoptions/perro2.jpg";
-import banner3 from "../assets/adoptions/perro3.jpg";
+import banner from "../assets/adoptions/perro1.png";
+import banner2 from "../assets/adoptions/perro2.png";
+import banner3 from "../assets/adoptions/perro3.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -16,7 +16,7 @@ const CarouselAdop = () => {
         autoplay: true,
     };
     return (
-        <div className='flex items-center max-w-sm md:max-w-lg lg:w-full mb-5 lg:mb-0 h-96'>
+        <div className='flex items-center max-w-sm md:max-w-lg lg:w-full mb-5 lg:mb-0'>
             <Slider {...settings} className='w-full mt-8'>
             <div>
                     <img src={banner} alt="adopciones my happy pet" />
--- src/views/Association/Adoption.jsx
+++ src/views/Association/Adoption.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import Header from '../../components/templates/Header';
 import Footer from '../../components/templates/Footer';
 import Photos from '../../helpers/CarouselAdop';
-import Text from '../../helpers/CarouselAdopText';
+import { Link } from 'react-router-dom';
+import points from '../../assets/points.svg'
 
 const Adoption = () => {
     return (
         <div className="bg-[#FAFAFA] w-full">
             <Header />
-            <div className='grid grid-flow-col place-content-center gap-20 PhotosPet'>
-            <Photos />
-                <div className="row-span-2 ..."><Text /></div>
+            <div className='flex justify-center mb-20 PhotosPet'>
+                <div className='text-black flex flex-1 justify-center'>
+                <Photos />
+                    <ul className='flex px-12 mb-4 gap-6 flex-col justify-center text-black'>
+                        <p className='w-full text-center text-2xl font-bold'>¿Te interesa alguna de las opciones?</p>
+                        <a href='https://www.instagram.com/cachorrilandia/?hl=es-la' target="_blank" relative="path" className='flex justify-center lg:justify-start hover:ml-3 transition-all duration-300 my-2'>
+                            <img src={points} loading='lazy' width={15} />
+                            <p className='ml-2 font-bold'>NALA - Cachorrilandia</p>
+                        </a>
+                        <a href='https://www.facebook.com/SantuarioAnimalAve/' target="_blank" relative="path" className='flex justify-center lg:justify-start hover:ml-3 transition-all duration-300 my-2'>
+                            <img src={points} loading='lazy' width={15} />
+                            <p className='ml-2 font-bold'>MAYA- Santuario Animal AVE</p>
+                        </a>
+                        <a href='https://www.facebook.com/ProyectoERAasociacion/' target="_blank" relative="path" className='flex justify-center lg:justify-start hover:ml-3 transition-all duration-300 my-2'>
+                            <img src={points} loading='lazy' width={15} />
+                            <p className='ml-2 font-bold'>KIRA- Proyecto ERA</p>
+                        </a>
+                    </ul>
+                </div>
             </div>
             <Footer />
         </div>
